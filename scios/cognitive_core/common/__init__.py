78dece0347578091c5729ec9323be59d80c6f85c
Bui Dinh Hoang <hbdatahoang@gmail.com>
2026-09-13 21:08:52 +0700

    refactor(cognitive-core): remove duplicate common CognitiveContext

--- scios/cognitive_core/common/__init__.py
+++ scios/cognitive_core/common/__init__.py
@@ -4,7 +4,6 @@ SciOS Cognitive Core - Common
 
 Foundational components shared across the Cognitive Core:
 - interfaces: Abstract base classes and protocols
-- context: CognitiveContext for passing state
 - result: CognitiveResult for standardized outputs
 - errors: Common exception hierarchy
 """
@@ -15,7 +14,6 @@ from scios.cognitive_core.common.interfaces import (
     ReasonerInterface,
     ToolInterface,
 )
-from scios.cognitive_core.common.context import CognitiveContext
 from scios.cognitive_core.common.result import CognitiveResult
 
 from scios.cognitive_core.common.errors import (
@@ -26,12 +24,12 @@ from scios.cognitive_core.common.errors import (
     ToolError,
     PipelineError,
 )
+
 __all__ = [
     "PlannerInterface",
     "MemoryInterface",
     "ReasonerInterface",
     "ToolInterface",
-    "CognitiveContext",
     "CognitiveResult",
     "CognitiveError",
     "PlannerError",
